541f68dd4afcf7d12f71fb2930bd98b5ca8bba78
Hayley Treutens <578003@student.belgiumcampus.ac.za>
2025-04-15 17:30:01 +0200

    Updated File Structure

--- src/preprocess_data.py
+++
@@ -1,7 +0,0 @@
-"""
-# This file contains the code for preprocessing the data.
-    A. Data Cleaning: 
-        -> Handling missing values
-        -> Removing duplicates
-        -> Outlier value Handling
-"""
